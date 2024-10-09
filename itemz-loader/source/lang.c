827709d336019811d8788a4a07e73a04c97ad638
Space V <40030799+ahcenezdh@users.noreply.github.com>
2024-10-09 02:59:26 +0200

    update
    forgot this was a c file and not a cpp file

--- itemz-loader/source/lang.c
+++ itemz-loader/source/lang.c
@@ -1,100 +1,134 @@
+
 #include "lang.h"
 #include "ini.h"
 #include "Header.h"
-#include <unordered_map>
-#include <string>
-#include <array>
-#include <memory>
 
-class LanguageManager {
-private:
-    std::array<std::string, LANG_NUM_OF_STRINGS> langStrings;
-    bool initialized = false;
-
-    LanguageManager() = default;
-
-    static int iniHandler(void* user, const char* section, const char* name, const char* value) {
-        auto* langManager = static_cast<LanguageManager*>(user);
-        if (strcmp(section, "LOADER") == 0) {
-            for (int i = 0; i < LANG_NUM_OF_STRINGS; ++i) {
-                if (strcmp(name, langKeyNames[i]) == 0) {
-                    langManager->langStrings[i] = value;
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
-
-public:
-    static LanguageManager& getInstance() {
-        static LanguageManager instance;
-        return instance;
-    }
-
-    bool initialize(int langCode) {
-        if (initialized) return true;
-
-        char langFilePath[100];
-        snprintf(langFilePath, sizeof(langFilePath), LANG_DIR, langCode);
-
-        int error = ini_parse(langFilePath, iniHandler, this);
-        if (error != 0) {
-            logshit("Bad config file (first error on line %d)!", error);
-            return false;
-        }
-
-        initialized = true;
-        return true;
-    }
-
-    const std::string& getLanguageString(Lang_STR str) const {
-        if (!initialized) {
-            static const std::string errorMsg = "ERROR: Lang has not loaded";
-            return errorMsg;
-        }
-
-        if (str >= LANG_NUM_OF_STRINGS) {
-            return langStrings[STR_NOT_FOUND];
-        }
-
-        return langStrings[str].empty() ? langStrings[STR_NOT_FOUND] : langStrings[str];
-    }
-
-    bool isInitialized() const { return initialized; }
-};
-
-// For backward compatibility
+LangStrings* stropts;
 bool lang_is_initialized = false;
-
 int32_t PS4GetLang() {
     int32_t lang;
     if (sceSystemServiceParamGetInt(1, &lang) != 0) {
-        logshit("Unable to get language code");
+        logshit("Unable to qet language code");
         return 0x1;
     }
     return lang;
 }
 
-const char* getLangSTR(Lang_STR str) {
-    return LanguageManager::getInstance().getLanguageString(str).c_str();
-}
+char* getLangSTR(enum Lang_STR str)
+{
 
-bool LoadLangs(int LangCode) {
-    auto& langManager = LanguageManager::getInstance();
-    if (langManager.initialize(LangCode)) {
-        lang_is_initialized = true;
-        return true;
+    if (lang_is_initialized) {
+        if (str > LANG_NUM_OF_STRINGS) {
+            return stropts->strings[STR_NOT_FOUND];
+        }
+        else
+        {
+            if (stropts->strings[str])
+                return stropts->strings[str];
+            else
+                return stropts->strings[STR_NOT_FOUND];
+        }
     }
-    msgok("LoadLangs failed");
-    return false;
+    else
+        return "ERROR: Lang has not loaded";
 }
 
-// Array of language key names for INI parsing
-const char* langKeyNames[LANG_NUM_OF_STRINGS] = {
-    "REINSTALL_PKG", "LOADER_ERROR", "MORE_INFO", "DOWNLOADING_UPDATE",
-    "INI_FAIL", "UPDATE_REQ", "UPDATE_APPLIED", "OPT_UPDATE",
-    "LOADER_FATAL", "FATAL_JB", "FATAL_REJAIL", "EXT_NOT_SUPPORTED",
-    "SWU_ERROR", "RSA_LOAD", "RSA_FAILED", "SECURE_FAIL",
-    "STR_NOT_FOUND", "PING_FAILED"
-};
+
+static int load_lang_ini(void* user, const char* section, const char* name,
+    const char* value)
+{
+    LangStrings* set = (LangStrings*)user;
+
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+
+    if (MATCH("LOADER", "REINSTALL_PKG")) {
+        snprintf(set->strings[REINSTALL_PKG], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "LOADER_ERROR")) {
+        snprintf(set->strings[LOADER_ERROR], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "MORE_INFO")) {
+        snprintf(set->strings[MORE_INFO], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "DOWNLOADING_UPDATE")) {
+        snprintf(set->strings[DOWNLOADING_UPDATE], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "INI_FAIL")) {
+        snprintf(set->strings[INI_FAIL], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "UPDATE_REQ")) {
+        snprintf(set->strings[UPDATE_REQ], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "UPDATE_APPLIED")) {
+        snprintf(set->strings[UPDATE_APPLIED], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "OPT_UPDATE")) {
+        snprintf(set->strings[OPT_UPDATE], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "LOADER_FATAL")) {
+        snprintf(set->strings[LOADER_FATAL], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "FATAL_JB")) {
+        snprintf(set->strings[FATAL_JB], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "FATAL_REJAIL")) {
+        snprintf(set->strings[FATAL_REJAIL], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "EXT_NOT_SUPPORTED")) {
+        snprintf(set->strings[EXT_NOT_SUPPORTED], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "SWU_ERROR")) {
+        snprintf(set->strings[SWU_ERROR], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "RSA_LOAD")) {
+        snprintf(set->strings[RSA_LOAD], 255, "%s", value);//
+    }
+    else if (MATCH("LOADER", "RSA_FAILED")) {
+        snprintf(set->strings[RSA_FAILED], 255, "%s", value);//
+    }
+    else if (MATCH("LOADER", "SECURE_FAIL")) {
+        snprintf(set->strings[SECURE_FAIL], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "STR_NOT_FOUND")) {
+        snprintf(set->strings[STR_NOT_FOUND], 255, "%s", value);
+    }
+    else if (MATCH("LOADER", "PING_FAILED")) {
+        snprintf(set->strings[PING_FAILED], 255, "%s", value);
+    }
+
+    return 1;
+
+}
+
+bool LoadLangs(int LangCode)
+{
+
+    stropts = (LangStrings*)malloc(sizeof(LangStrings));
+    if (stropts != NULL) {
+        for (int i = 0; i < LANG_NUM_OF_STRINGS; i++) {
+            stropts->strings[i] = calloc(256, sizeof(char));
+
+            if (stropts->strings[i] == NULL) {
+                msgok("Failed to load Language(s)... Failed to allocate stropts->strings[%i]", i);
+                return false;
+            }
+        }
+
+        char tmp[100];
+        snprintf(tmp, 99, LANG_DIR, LangCode);
+
+        int error = ini_parse(tmp, load_lang_ini, stropts);
+        if (error) {
+            logshit("Bad config file (first error on line %d)!", error);
+            return false;
+        }
+        else
+            lang_is_initialized = true;
+    }
+    else{
+        msgok("LoadLangs failed");
+        return false;
+    }
+
+    return true;
+}
